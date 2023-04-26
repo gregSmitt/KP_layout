03a052eccdcbd2c294766e197d7d4d89ad742a10
gregSmitt <89312058+gregSmitt@users.noreply.github.com>
2023-04-26 15:07:39 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+(() => {
+    "use strict";
+})
+//! flsFunctions.spollers();
+//! import * as flsForms from "./files/forms/forms.js";
+//! import * as flsScroll from "./files/scroll/scroll.js";
+();
